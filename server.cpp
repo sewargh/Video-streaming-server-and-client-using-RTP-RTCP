e4895ac6e9e12dfcf67fdfe44869c908b5cc5a3f
sewargh <106510817+sewargh@users.noreply.github.com>
2022-05-31 16:52:30 +0300

    Add files via upload

--- server.cpp
+++ server.cpp
@@ -25,7 +25,7 @@ struct RTPpkt
     int Timestamp;
     int SSRC;
     char data[10000];
-};
+}ACKrtp;
 // takes one argument which is the port number
 int main(int argc, char const *argv[])
 {
@@ -61,42 +61,60 @@ int main(int argc, char const *argv[])
         return 0;
     }
     // puts the server socket in a passive mode, where it waits for the client to approach the server to make a connection.
-    RTPpkt ACKrtp;
     memset(&ACKrtp, 0, sizeof(ACKrtp));
-    ACKrtp.CC |= 1UL << 0;
-    ACKrtp.CC |= 1UL << 1;
-
     memcpy(ACKrtp.data, "Hello from Server !\n", sizeof(ACKrtp.data));
 
+    ACKrtp.V &= ~(1UL << 0);
+    ACKrtp.V |= 1UL << 1;
+    cout << "V2 = " << ACKrtp.V << endl;
+    ACKrtp.P &= ~(1UL << 0);
+    cout << "P0 = " << ACKrtp.P << endl;
+    ACKrtp.X &= ~(1UL << 0);
+    cout << "X0 = " << ACKrtp.X << endl;
+    ACKrtp.CC &= ~(1UL << 0);
+    ACKrtp.CC &= ~(1UL << 1);
+    ACKrtp.CC &= ~(1UL << 2);
+    ACKrtp.CC &= ~(1UL << 3);
+    cout << "CC0 = " << ACKrtp.CC << endl;
+    ACKrtp.M &= ~(1UL << 0);
+    cout << "M0 = " << ACKrtp.M << endl;
+    ACKrtp.PT &= ~(1UL << 0);
+    ACKrtp.PT |= 1UL << 1;
+    ACKrtp.PT &= ~(1UL << 2);
+    ACKrtp.PT |= 1UL << 3;
+    ACKrtp.PT |= 1UL << 4;
+    ACKrtp.PT &= ~(1UL << 5);
+    ACKrtp.PT &= ~(1UL << 6);
+    cout << "PT26 = " << ACKrtp.PT << endl;
+    ACKrtp.SeqNum = 258;
+    cout << "SeqNum = " << ACKrtp.SeqNum << endl;
+
     /******** Listen To The Port to Any Connection **********/
-    //while (1)
-    
-        // Block until receive message from a client
-        ClientAddLen = sizeof(ClientAddr);
-        int BytesRead;
-        cout << "Waiting on port " << PortNum << endl;
-        BytesRead = recvfrom(Socket_Server, Buffer, 10000, 0, (struct sockaddr *)&ClientAddr, &ClientAddLen);
-        Buffer[BytesRead] = 0;
-        cout << "#of read bytes = " << BytesRead << "\nData is : -" << Buffer << endl;
-        sendto(Socket_Server, &ACKrtp, sizeof(ACKrtp), 0, (struct sockaddr *)&ClientAddr, ClientAddLen);
+    // while (1)
 
-        // CImgDisplay disp;
-        for (int i = 1; i <= 500; i++)
+    // Block until receive message from a client
+    ClientAddLen = sizeof(ClientAddr);
+    int BytesRead;
+    cout << "Waiting on port " << PortNum << endl;
+    BytesRead = recvfrom(Socket_Server, Buffer, 10000, 0, (struct sockaddr *)&ClientAddr, &ClientAddLen);
+    Buffer[BytesRead] = 0;
+    cout << "#of read bytes = " << BytesRead << "\nData is : -" << Buffer << endl;
+    sendto(Socket_Server, &ACKrtp, sizeof(ACKrtp), 0, (struct sockaddr *)&ClientAddr, ClientAddLen);
+
+    for (int i = 1; i <= 500; i++)
+    {
+        char name[1000];
+
+        sprintf(name, "vid/image%03d.jpg", i);
+        CImg<unsigned char> img(name);
+        if (sendto(Socket_Server, img, sizeof(img), 0, (struct sockaddr *)&ServerAddr, (socklen_t)AddrLen) == -1)
         {
-            char name[1000];
-
-            sprintf(name, "vid/image%03d.jpg", i);
-            CImg<unsigned char> img(name);
-            if (sendto(Socket_Server, img, sizeof(img), 0, (struct sockaddr *)&ServerAddr, (socklen_t)AddrLen) == -1)
-            {
-                cout << "Error in sending frame #" << i << " SORRY!\n"
-                     << strerror(errno) << "\n**** EXIT ****\n";
-                return 0;
-            }
-            // disp=img;
-            usleep(30000);
+            cout << "Error in sending frame #" << i << " SORRY!\n"
+                 << strerror(errno) << "\n**** EXIT ****\n";
+            return 0;
         }
-    
+        usleep(30000);
+    }
 
     // static int HeaderSize = 12;
     // cout << "Create thread \n";
