51150525469dc6439f0a5f16d772c6f5494abe80
sewargh <106510817+sewargh@users.noreply.github.com>
2022-06-02 03:55:17 +0300

    Add files via upload

---
+++ initialize.h
@@ -0,0 +1,16 @@
+#include "struct.h"
+
+/*//rtcp server
+int numpktsent = 0;
+int numoctsent = 0;
+bool flag = false;*/
+
+/*//rtcp client
+int numpktsent = 0;
+int numoctsent = 0;
+bool flag = false;
+int sts[500];
+int rts[500];
+int d;
+int j = 0;*/
+//rtcp
