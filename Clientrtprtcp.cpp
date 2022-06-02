d570b173197f79cbcac60d2ec193fd2eddc1c093
R7maSOS <106558318+R7maSOS@users.noreply.github.com>
2022-06-02 23:40:40 +0300

    Update Clientrtprtcp.cpp

--- Clientrtprtcp.cpp
+++ Clientrtprtcp.cpp
@@ -100,8 +100,8 @@ void *rtp_client_func(void *PORT)
         fclose(writeFile);
         pthread_mutex_lock(&mutex);
         numpktsent++;
-        sts[index_s] = Receivedpkt.Timestamp;
-        rts[index_r] = time(NULL);
+        sts[index_s++] = Receivedpkt.Timestamp;
+        rts[index_r++] = time(NULL);
         numpktsend5++;
         pthread_mutex_unlock(&mutex);
     }
@@ -109,12 +109,19 @@ void *rtp_client_func(void *PORT)
     shutdown(Socket_Client, SHUT_RDWR);
     return 0;
 }
-void *rtcp_client_func(void *PORTNUM)
+include "initialize.h"
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+// rtcp client
+int numpktsent = 0;
+int sts[500];
+int rts[500];
+int index_s = 0;
+int index_r = 0;
+int numpktsend5 = 0;
+
+void *rtp_client_func(void *PORT)
 {
-    bool flag = false;
-    int d = 0;
-    int j = 0;
-    int PortNumServer = (int)(intptr_t)PORTNUM;
+    int PortNumServer = (int)(intptr_t)PORT;
     int opt = 1;
     struct sockaddr_in ClientAddr, ServerAddr; // socket file descriptor for the server to connect to.
     int AddrLen = sizeof(ClientAddr);
@@ -122,23 +129,26 @@ void *rtcp_client_func(void *PORTNUM)
     int Socket_Client = socket(AF_INET, SOCK_DGRAM, 0);
     if (Socket_Client == -1)
     {
-        cout << "Error in creation socket.\n**** EXIT ****\n";
+        cout << "Error in socket creation.\n**** EXIT ****\n";
         return 0;
     }
 
     memset(&ServerAddr, 0, sizeof(ServerAddr));
     ServerAddr.sin_family = AF_INET;
     ServerAddr.sin_addr.s_addr = INADDR_ANY;
-    // inet_pton(AF_INET, argv[1], &ServerAddr.sin_addr);
     ServerAddr.sin_port = htons(PortNumServer);
+    // struct timeval tv;
+    // fd_set rfds;
+    /*FD_ZERO(&rfds);
+    FD_SET(Socket_Client, &rfds);
+    tv.tv_usec = 0.0;
+    int recVal = 0;
+    bool timePassed = false;
+    time_t startListenTime = time(NULL);
+    tv.tv_sec = 30000;*/
+
     int BytesRead, FrameRead;
     AddSenderLen = sizeof(ServerAddr);
-    char request[] = "Hello";
-    sendto(Socket_Client, request, sizeof(request), 0, (struct sockaddr *)&ServerAddr, (socklen_t)AddSenderLen);
-
-    memset(&rtcpclient_rr, 0, sizeof(rtcpclient_rr));
-    memset(sts, 0, 500 * sizeof(*sts));
-    memset(rts, 0, 500 * sizeof(*rts));
     {
         rtcpclient_rr.version &= ~(1UL << 0);
         rtcpclient_rr.version |= 1UL << 1;
@@ -171,32 +181,35 @@ void *rtcp_client_func(void *PORTNUM)
         recvfrom(Socket_Client, &rtcpclient_sr, sizeof(rtcpclient_sr), 0, (struct sockaddr *)&ServerAddr, &AddSenderLen);
         stlsr = time(NULL);
         cout << "SR packet \n"
-             << "Version :" << rtcpclient_sr.version << "   Padding :" << rtcpclient_sr.p << "        Reciever report count :" << rtcpclient_sr.rcount << "     Packet type :" << rtcpclient_sr.pt << endl;
-        cout << "Lenght(in words) :" << rtcpclient_sr.wordlen << "  SSRC :" << rtcpclient_sr.ssrc << "        NTP timestamp:" << rtcpclient_sr.ntp_secfrac << " RTP timestamp :" << rtcpclient_sr.rtp_ts << endl;
-        cout << "NO. of packets sent :" << rtcpclient_sr.pksent << "        NO. of octets sent :" << rtcpclient_sr.octsent << endl;
+             << "|Version :" << rtcpclient_sr.version << "\t|Padding :" << rtcpclient_sr.p << "\t|Reciever report count :" << rtcpclient_sr.rcount << "\t|Packet type :" << rtcpclient_sr.pt << endl;
+        cout << "|Lenght(in words) :" << rtcpclient_sr.wordlen << "\t|SSRC :" << rtcpclient_sr.ssrc << "\t|NTP timestamp:" << rtcpclient_sr.ntp_secfrac<<endl;
+        cout << "|RTP timestamp :" << rtcpclient_sr.rtp_ts << "\t|NO. of packets sent :" << rtcpclient_sr.pksent << "\t|NO. of octets sent :" << rtcpclient_sr.octsent << endl;
         cout << endl
              << endl;
         if (flag == true)
         {
             recvfrom(Socket_Client, &rtcpclient_bye, sizeof(rtcpclient_bye), 0, (struct sockaddr *)&ServerAddr, &AddSenderLen);
             cout << "BYE packet \n"
-                 << "Version :" << rtcpclient_bye.version << "   Padding :" << rtcpclient_bye.p << "        Source count :" << rtcpclient_bye.scount << "     Packet type :" << rtcpclient_bye.pt << endl;
-            cout << "Lenght(in words) :" << rtcpclient_bye.wordlen << "  SSRC :" << rtcpclient_bye.ssrc << endl;
+                 << "|Version :" << rtcpclient_bye.version << "\t|Padding :" << rtcpclient_bye.p << "\t|Source count :" << rtcpclient_bye.scount << "\t|Packet type :" << rtcpclient_bye.pt << endl;
+            cout << "|Lenght(in words) :" << rtcpclient_bye.wordlen << "\t|SSRC :" << rtcpclient_bye.ssrc << endl;
             cout << endl
                  << endl;
         }
-        for (int i = numpktsent - 1; i <= numpktsent; i++)
-        {
-            if (sts[i] != 0 && sts[i + 1] != 0 && rts[i] != 0 && rts[i + 1] != 0)
-                d = rts[i + 1] - sts[i + 1] - rts[i] + sts[i];
-            j = ((abs(d) - j) / 16) + j;
-        }
-        rtcpclient_rr.jitter = j;
-        rtcpclient_rr.bfraction = ((166 - numpktsent) / 166) * 256;
-        rtcpclient_rr.blost = rtcpclient_rr.blost + (166 - numpktsend5);
+        pthread_mutex_lock(&mutex);
 
+            if (sts[numpktsent - 2] != 0 && sts[numpktsent - 1] != 0 && rts[numpktsent - 2] != 0 && rts[numpktsent - 1] != 0){
+                //cout<<rts[numpktsent - 2]<<"\t"<<sts[numpktsent - 2]<<"\t"<<rts[numpktsent - 1]<<"\t"<<sts[numpktsent - 1]<<endl;
+                d = rts[numpktsent - 2] - sts[numpktsent - 2] - rts[numpktsent - 1] + sts[numpktsent - 1];
+                j = float(((abs(d) - j) / 16) + j); }
+        rtcpclient_rr.jitter = j;
+        rtcpclient_rr.bfraction = float(((166 - numpktsend5) / 166) * 256);
+        rtcpclient_rr.blost += (166 - numpktsend5);
+        if (n==3) rtcpclient_rr.blost += (2 - numpktsend5);
         rtcpclient_rr.lsr = rtcpclient_sr.ntp_secfrac - 2208988800;
-        rtcpclient_rr.dlsr = (time(NULL) - stlsr) / 65536;
+        rtcpclient_rr.dlsr = float((time(NULL) - stlsr) / 65536);
+        n++
+        pthread_mutex_unlock(&mutex);
+
 
         if (sendto(Socket_Client, &rtcpclient_rr, sizeof(rtcpclient_rr), 0, (struct sockaddr *)&ServerAddr, (socklen_t)AddrLen) == -1)
         {
@@ -213,6 +226,8 @@ void *rtcp_client_func(void *PORTNUM)
     shutdown(Socket_Client, SHUT_RDWR);
     return 0;
 }
+
+
 // server ip, server port, XXXXXX no XXXXXX video name
 int main(int argc, char const *argv[])
 {
